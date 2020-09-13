2436f46b9f8701ce4352adfbc15dc732e9560fc3
kleberjuniorr63 <47255287+kleberjuniorr63@users.noreply.github.com>
2020-09-13 01:18:01 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $logado=$_GET['logado'];
     <body>
   <div class="container">
     <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div class="col-sm-9 mt-5 col-md-7 col-lg-5 mx-auto">
         <div class="card card-signin my-5">
           <div class="card-body">
               <center></center>
@@ -47,11 +47,11 @@ $logado=$_GET['logado'];
                 <label for="inputPassword">Senha</label>
               </div>
               <br>
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
+              <button class="btn btn-lg btn-success btn-block text-uppercase" type="submit">Entrar</button>
             </form>
             <br>
             <form action="cadastro.php">
-              <button class="btn btn-lg btn-secondary btn-block text-uppercase" type="submit">Cadastrar</button>
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Cadastrar</button>
             </form>
           </div>
         </div>
